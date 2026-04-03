c724064be78dab1d30264c3b637d607e224d387e
Shivam Pimple <shivampimple29@gmail.com>
2026-04-03 21:20:00 +0530

    Basic backend setup

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const mongoose = require("mongoose");
+const DB_URL = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
+    console.log("Connection with DB is successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(DB_URL);
+}
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).json({ error: message });
+});
+
+app.listen(port, () => {
+  console.log("App listening at port 8000");
+});
